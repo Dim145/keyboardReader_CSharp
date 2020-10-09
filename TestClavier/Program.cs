0c12a9087759782a936ccf9793673a336ab423a3
Dim145 <50747004+Dim145@users.noreply.github.com>
2020-10-09 15:32:59 +0200

    Add Test project

---
+++ TestClavier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lecture;
+
+namespace TestClavier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Saisie String = ");
+            String s = Clavier.lireString();
+            Console.WriteLine("Resultat = " + s);
+
+            Console.Write("Saisie int = ");
+            int i = Clavier.lire_int();
+            Console.WriteLine("Resultat = " + i);
+
+            Console.Write("Saisie bool = ");
+            bool b = Clavier.lire_boolean();
+            Console.WriteLine("Resultat = " + b);
+        }
+    }
+}
