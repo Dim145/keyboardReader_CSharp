dd2fe82397b82830c954297679784f1914d8efd7
Dim145 <50747004+Dim145@users.noreply.github.com>
2020-10-14 20:08:01 +0200

    Ajout des types manquants

--- TestClavier/Program.cs
+++ TestClavier/Program.cs
@@ -17,6 +17,14 @@ namespace TestClavier
             int i = Clavier.lire_int();
             Console.WriteLine("Resultat = " + i);
 
+            Console.Write("Saisie float = ");
+            float f = Clavier.lire_float();
+            Console.WriteLine("Resultat = " + f);
+
+            Console.Write("Saisie byte = ");
+            sbyte by = Clavier.lire_sbyte();
+            Console.WriteLine("Resultat = " + by);
+
             Console.Write("Saisie bool = ");
             bool b = Clavier.lire_boolean();
             Console.WriteLine("Resultat = " + b);
